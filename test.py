525818e538b610537d8dc2bca16a61398ec308c1
Spencer Potts <spencerjpotts@gmail.com>
2017-11-04 19:04:00 +1000

    Added While loop

--- test.py
+++ test.py
@@ -11,22 +11,25 @@ user = User('X-AUTH-TOKEN')
 
 print("[!] Welcome %s" % user.name())
 
-# collect list of newly added nearby matches and put them in matches var
-matches = user.subjects()  # user.subjects() returns array of match objects
 
 # Iterate over each individual user in matches[] and pass their id into the like def
-for match in matches:
+while True:
 
-    # Like user with ID
-    user.like(match['_id'])
+    # collect list of newly added nearby matches and put them in matches var
+    matches = user.subjects()  # user.subjects() returns array of match objects
 
-    # Print name of the user just liked and print a message ''EMPTY BIO'' if the persons bio was empty
-    print("""
-    -----------------------------
-    Name: %s
-    Bio: %s
-    -----------------------------
-    """ % (match['name'], match['bio'] if len(match['bio']) > 0 else "EMPTY BIO"))
+    for match in matches:
 
-    # Sleep for one second
-    time.sleep(1)
+        # Like user with ID
+        user.like(match['_id'])
+
+        # Print name of the user just liked and print a message ''EMPTY BIO'' if the persons bio was empty
+        print("""
+        -----------------------------
+        Name: %s
+        Bio: %s
+        -----------------------------
+        """ % (match['name'], match['bio'] if len(match['bio']) > 0 else "EMPTY BIO"))
+
+        # Sleep for one second
+        time.sleep(1)
