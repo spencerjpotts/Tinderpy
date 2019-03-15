5f2fd049af9962c0c8da501b5459870cb2b3d1cb
Spencer <spencerjpotts@gmail.com>
2019-03-15 22:55:01 +1100

    Quick fix up.

--- test.py
+++ test.py
@@ -7,32 +7,23 @@ import time
 import tinderpy
 
 
-user = tinderpy.User('TOKEN HERE')
+user = tinderpy.User('X-AUTH-TOKEN')
 
 print("[!] Welcome %s" % user.name())
 
-# collect list of newly added nearby matches and put them in matches var
+while True:
+    matches = user.discovery()  # user.subjects() returns array of match objects
+    for match in matches:
+        # Like user with ID
+        user.like(match['_id'])
 
-matches = user.discovery()  # user.subjects() returns array of match objects
+        # Print name of the user just liked and print a message ''EMPTY BIO'' if the persons bio was empty
+        print("""
+           -----------------------------
+           Name: %s
+           Bio: %s
+           -----------------------------
+           """ % (match['name'], match['bio'] if len(match['bio']) > 0 else "EMPTY BIO"))
 
-for match in matches:
-    # Like user with ID
-    user.like(match['_id'])
-
-    # Print name of the user just liked and print a message ''EMPTY BIO'' if the persons bio was empty
-    print("""
-       -----------------------------
-       Name: %s
-       Bio: %s
-       -----------------------------
-       """ % (match['name'], match['bio'] if len(match['bio']) > 0 else "EMPTY BIO"))
-
-    # Sleep for one second
-    time.sleep(1)
-
-
-# Message example.
-for match in user.matches(count=1):
-    print("You messaged ", match['person']['name'])
-    msg_info = user.message_match(match['person']['_id'], "Hello.")
-    print(msg_info)
+        # Sleep for one second
+        time.sleep(1)
